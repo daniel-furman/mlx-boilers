e6c14b50ad9f674881a2df2248af08af88aab631
daniel-furman <dryanfurman@gmail.com>
2024-05-27 11:31:17 -0700

    latest code

--- scripts/prompt1script.zsh
+++ scripts/prompt1script.zsh
@@ -8,11 +8,28 @@ echo "Hostname: $(hostname)"
 
 ##############################################
 
-prompt="What is your favorite color?"
-echo "Prompt:" $prompt
+# This line sets the prompt
+prompt="What is your favorite movie?"
 
+# This line prints the prompt
+# echo "Prompt:" $prompt
+
+# List of models
+# mlx-community/openchat-3.6-8b-20240522-4bit
+# dfurman/Mistral-7B-Instruct-v0.3-mlx-4bit
+# mlx-community/Meta-Llama-3-8B-Instruct-4bit
+# mlx-community/aya-23-8B-4bit
+# mlx-community/Phi-3-mini-128k-instruct-4bit
+
+# This line sets the model
+model="mlx-community/openchat-3.6-8b-20240522-4bit"
+
+# This line prints the model
+# echo "Model:" $model
+
+# This line runs the model on the prompt
 python3 -m mlx_lm.generate \
-            --model "mlx-community/Meta-Llama-3-8B-Instruct-4bit" \
+            --model $model \
             --prompt $prompt \
             --max-tokens 200 \
             --temp 0.6 \
